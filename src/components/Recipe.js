b7f0fa56fe0a65aa030210b9b4cda27727a26492
Noortje Adolfs <noortje.adolfs@novi-education.nl>
2023-11-19 21:28:24 +0100

    add type submit to button

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -11,7 +11,7 @@ const Recipe = ({ recipe }) => {
             <h2>{label}</h2>
             <img src={image} alt={label} />
         </a>
-      <button onClick={() => setShow(!show)}>Ingredients</button>
+      <button type="submit" onClick={() => setShow(!show)}>Ingredients</button>
       {show && <RecipeDetails ingredients={ingredients} />}
     </article>
   );
